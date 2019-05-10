2b020bab46b7723c14f1b60e15d2cc9b764c9c35
SarahWuTX <xlksarah@qq.com>
2019-05-10 20:09:07 +0800

    update proxy

--- movie-frontend/vue.config.js
+++ movie-frontend/vue.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/MovieWeb/" : "/",
   devServer: {
-    port: 8081,
     proxy: {
       "/": {
-        port: 8081,
         target: "http://122.152.221.44:8888",
         // ws: true,
         changeOrigin: true
